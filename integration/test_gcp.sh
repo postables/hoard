ac3718729c1ae3bc6aa30d64529f60e7a2ec6633
greg <greg.hill@monax.io>
2018-10-16 00:33:03 +0100

    key file loc
    Signed-off-by: greg <greg.hill@monax.io>

--- integration/test_gcp.sh
+++ integration/test_gcp.sh
@@ -17,7 +17,7 @@ GCS_PREFIX="hoard-integration-test"
 cd "$(dirname "$0")"
 
 # For CI we expect this to be set
-export GOOGLE_APPLICATION_CREDENTIALS="/gcloud-service-key.json"
+export GOOGLE_APPLICATION_CREDENTIALS="${HOME}/gcloud-service-key.json"
 
 read -d '' HOARD_JSON_CONFIG << CONFIG
   {
@@ -45,6 +45,8 @@ echo ${HOARD_JSON_CONFIG} | jq '.'
 # Delete existing storage
 echo "Deleting existing GCS backing store..."
 gsutil rm "gs://${GCS_BUCKET}/${GCS_PREFIX}/**"
+
+set +e
 
 # Build unless asked not to
 echo "Bringing up integration test containers with docker-compose..."
